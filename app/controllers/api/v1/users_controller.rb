2c8f80d5f79c7dfac929ca1baf7edf9fecf19bb8
Abdoul Diallo <committed1@gmail.com>
2019-02-19 17:37:51 +0200

    Add user controller with crud actions

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :find_user, only: [:update]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+end
