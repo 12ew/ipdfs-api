362f392e62e0081ea6b5866fa6cb564e36079cae
Abdoul Diallo <committed1@gmail.com>
2019-03-04 20:31:50 +0200

    Add author field to book model

---
+++ db/migrate/20190304171900_add_author_to_book.rb
@@ -0,0 +1,5 @@
+class AddAuthorToBook < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :author, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_143202) do
+ActiveRecord::Schema.define(version: 2019_03_04_171900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_143202) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "file"
+    t.string "author"
   end
 
   create_table "genres", force: :cascade do |t|
