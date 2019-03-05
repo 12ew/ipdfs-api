7ed77f516f09166f550ac494926273980901120f
Abdoul Diallo <committed1@gmail.com>
2019-03-05 18:46:02 -0500

    Add genre field to book model

---
+++ db/migrate/20190305204615_add_genre_to_book.rb
@@ -0,0 +1,5 @@
+class AddGenreToBook < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :genre, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_171900) do
+ActiveRecord::Schema.define(version: 2019_03_05_204615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_171900) do
     t.string "image"
     t.string "file"
     t.string "author"
+    t.string "genre"
   end
 
   create_table "genres", force: :cascade do |t|
