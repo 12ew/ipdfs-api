bf6b4838b465e40bfcad2a72da160901fc548d3a
Abdoul Diallo <committed1@gmail.com>
2019-04-17 20:50:36 -0400

    Update CORS settings

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module IpdfsApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, :methods => [:get, :post, :options, :delete, :put, :patch], credentials: true
       end
     end
 
